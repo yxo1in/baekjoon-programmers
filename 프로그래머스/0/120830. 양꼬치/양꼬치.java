aefe24167ca4fa0412b54b5a5c70bc0482899f22
yungijin09 <s2546@e-mirim.hs.kr>
2025-10-28 01:34:52 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(int n, int k) {
         int answer = 0;
         
-        answer = 12000*n + (k - n/10) * 2000 ;
+        answer = 12000*n + (k - n/10) * 2000 ; // 음료수의 개수 - (양꼬치 개수 /10)*2000
         return answer;
     }
 }
