dc56d494245faab8f93a9439ac6dd787aea7b9d8
yungijin09 <s2546@e-mirim.hs.kr>
2025-10-28 01:37:50 +0900

    [level 0] Title: 제곱수 판별하기, Time: 4.09 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int i = 1;
+        while(true){
+            if(i < n){
+                if(Math.pow(i, 2) == n){
+                    return answer = 1;           
+                }
+            }
+            else
+                return answer = 2;
+            i++;
+        }
+    }
+}
