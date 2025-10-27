194d753a3b5d583cab8ffeea603830f7de83729a
yungijin09 <s2546@e-mirim.hs.kr>
2025-10-28 01:41:57 +0900

    [level 0] Title: 두 수의 곱 구하기, Time: 0.07 ms, Memory: 93.3 MB -BaekjoonHub

--- 프로그래머스/0/120804. 두 수의 곱 구하기/두 수의 곱 구하기.java
+++ 프로그래머스/0/120804. 두 수의 곱 구하기/두 수의 곱 구하기.java
@@ -1,7 +1,6 @@
 class Solution {
     public int solution(int num1, int num2) {
-        int answer;
-        answer = num1 * num2;
+        int answer = num1*num2;
         return answer;
     }
 }
