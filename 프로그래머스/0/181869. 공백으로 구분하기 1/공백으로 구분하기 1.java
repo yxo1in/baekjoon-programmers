187a23b800f6c5ff12570b7864a4e0b95c8c40d8
yungijin09 <s2546@e-mirim.hs.kr>
2026-03-03 09:52:38 +0900

    [level 0] Title: 공백으로 구분하기 1, Time: 0.58 ms, Memory: 84.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
@@ -0,0 +1,12 @@
+import java.util.StringTokenizer;
+
+class Solution {
+    public String[] solution(String my_string) {
+        StringTokenizer stk = new StringTokenizer(my_string, " ");
+        String[] answer = new String[stk.countTokens()];
+        for(int i = 0; i < answer.length; i++){
+            answer[i] = stk.nextToken();
+        }
+        return answer;
+    }
+}
