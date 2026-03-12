bbbacda84bad0280dc0e0c903bfb642582fa1434
yungijin09 <s2546@e-mirim.hs.kr>
2026-03-12 11:09:11 +0900

    [level 0] Title: 코드 처리하기, Time: 646.46 ms, Memory: 403 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String solution(String code) {
+        String answer = "";
+        int mode = 0;
+        for(int i = 0; i < code.length(); i++){
+            if(mode == 0){
+                if(code.charAt(i) == ('1')){
+                    mode = 1;
+                }
+                else{
+                    if(i % 2 == 0){
+                        answer += code.charAt(i);
+                    }
+                }
+            }
+            else{
+                if(code.charAt(i) == '1'){
+                    mode = 0;
+                }
+                else{
+                    if(i % 2 == 1){
+                        answer += code.charAt(i);
+                    }
+                }
+            }
+            System.out.println(mode);
+        }
+        if(answer.equals("")){
+            return "EMPTY";
+        }
+        return answer;
+    }
+}
