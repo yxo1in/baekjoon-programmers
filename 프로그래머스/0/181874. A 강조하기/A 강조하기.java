7f84b6ef99026f85fc55e681550ca31cff199594
yungijin09 <s2546@e-mirim.hs.kr>
2026-03-15 22:51:34 +0900

    [level 0] Title: A 강조하기, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -1,16 +1,17 @@
 class Solution {
     public String solution(String myString) {
-        String answer = "";
-        char[] ch = myString.toCharArray();
-        for(int i = 0; i < ch.length; i++){
-            if(ch[i] == 'a'){
-                ch[i] = 'A';
-            }
-            else if(Character.isUpperCase(ch[i]) == true && ch[i] != 'A'){
-                ch[i] = Character.toLowerCase(ch[i]);                     
-            }
-            answer += ch[i];
-        }
-        return answer;
+        // String answer = "";
+        // char[] ch = myString.toCharArray();
+        // for(int i = 0; i < ch.length; i++){
+        //     if(ch[i] == 'a'){
+        //         ch[i] = 'A';
+        //     }
+        //     else if(Character.isUpperCase(ch[i]) == true && ch[i] != 'A'){
+        //         ch[i] = Character.toLowerCase(ch[i]);                     
+        //     }
+        //     answer += ch[i];
+        // }
+        // return answer;
+        return  myString.toLowerCase().replace('a', 'A');
     }
 }
