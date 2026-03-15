4b7901b37b4a09868b8e8ef751efe90ea3d8a9ae
yungijin09 <s2546@e-mirim.hs.kr>
2026-03-15 22:50:34 +0900

    [level 0] Title: A 강조하기, Time: 10.24 ms, Memory: 89 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        char[] ch = myString.toCharArray();
+        for(int i = 0; i < ch.length; i++){
+            if(ch[i] == 'a'){
+                ch[i] = 'A';
+            }
+            else if(Character.isUpperCase(ch[i]) == true && ch[i] != 'A'){
+                ch[i] = Character.toLowerCase(ch[i]);                     
+            }
+            answer += ch[i];
+        }
+        return answer;
+    }
+}
