ca014245013bac9b3e4fd5de7428193f25a44137
yungijin09 <s2546@e-mirim.hs.kr>
2026-03-18 09:27:08 +0900

    [level 0] Title: 7의 개수, Time: 0.04 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        for(int i = 0; i < array.length; i++){
+            String str = Integer.toString(array[i]);
+            for(int j = 0; j < str.length(); j++){
+                if(str.charAt(j) == '7'){
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+}
