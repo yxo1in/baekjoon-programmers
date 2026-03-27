3b27b62f74b3a603e94ce11a2fe07a41a880023b
yungijin09 <s2546@e-mirim.hs.kr>
2026-03-27 14:28:34 +0900

    [level 0] Title: 합성수 찾기, Time: 0.26 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int cnt = 0;
+        for(int i = 4; i <= n; i++){
+            cnt = 0;
+            for(int j = 1; j <= n; j++){
+                if(i % j == 0){
+                    cnt++;
+                }
+            }
+            if(cnt >= 3){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
